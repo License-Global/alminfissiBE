35356a52347d3e036b6a3dae0b15e51b47b68ef8
License-Global <info@licenseglobal.it>
2024-07-07 17:33:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "luna",
+    dbName: "alminfissi",
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
